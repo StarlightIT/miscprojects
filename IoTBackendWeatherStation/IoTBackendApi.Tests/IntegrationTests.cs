92ae7c60a61e2e1f17e0d01474b6f5492b97ceff
James Huang <pdt.james.huang@gmail.com>
2018-11-28 00:35:08 +0100

    Added new test project

---
+++ IoTBackendWeatherStation/IoTBackendApi.Tests/IntegrationTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace IoTBackendApi.Tests
+{
+    public class IntegrationTests
+    {
+        public ServiceCollection Services;
+        public ServiceProvider ServiceProvider;
+        public IConfiguration Config;
+
+        [SetUp]
+        public void Setup()
+        {
+            Services = new ServiceCollection();
+            ConfigureServices(Services);
+            ServiceProvider = Services.BuildServiceProvider();
+        }
+
+        protected void ConfigureServices(IServiceCollection services)
+        {
+            Config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .AddEnvironmentVariables().Build();
+            services.AddOptions();
+        }
+    }
+}
